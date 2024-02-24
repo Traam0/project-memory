de13fb8ebc29d36794a43c7273d170833d7ea2da
traam0 <younesmrx@outlook.com>
2024-02-24 16:09:52 +0100

    base

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        'text': '#140d1c',
+        'background': '#f9f7fd',
+        'primary': '#631494',
+        'secondary': '#ba8ee6',
+        'accent': '#bf88b2',
+       },
+       
     },
   },
   plugins: [],
